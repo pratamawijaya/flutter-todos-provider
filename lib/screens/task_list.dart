09917c845e904498136f3a5fc0dcd7442f534ecf
pratamawijaya <set.mnemonix@gmail.com>
2019-11-19 11:06:42 +0700

    3 - create task list item widget

---
+++ lib/screens/task_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:todos_provider/models/Task.dart';
+
+class TaskList extends StatelessWidget {
+  final List<Task> tasks;
+
+  TaskList({@required this.tasks});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
